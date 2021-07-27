803918bc121eb4150133cb7839026c03c17bf010
Alperen KAÇMAZ <alperen703.akm@gmail.com>
2021-07-27 14:19:56 +0300

    controller error fixed

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-       $product = DB::table('products')->get();
+        $products = Product::all();
 
-        return view('product.index', ['products' => $product]);
+
+        // return view('welcome', ['products' => $products]);
+        return view('welcome', compact('products'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,26 +35,7 @@
 
 
 
-    <div class="col py-3">
-        <div class="row">
-            @foreach (DB::table('products')->get() as $prod)
 
-                <div class="col-4">
-                    <div class="card border-success mb-3" style="max-width: 18rem;">
-                        <div class="card-header bg-transparent border-success">{{ $prod->category }}</div>
-                        <div class="card-body text-success">
-                            <h4 class="card-title">{{ $prod->name }}</h4>
-                            <h6 class="card-title">Stock: {{ $prod->stock }}</h6>
-
-                        </div>
-                        <div class="card-footer bg-transparent border-success">Cost: ${{ $prod->price }}
-                        </div>
-                    </div>
-                </div>
-
-            @endforeach
-        </div>
-    </div>
 
 
     <div class="container-fluid">
@@ -72,40 +53,7 @@
                                 <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-speedometer2"></i> <span
-                                    class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-                            <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                                <li class="w-100">
-                                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-                        </li>
-                        <li>
-                            <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                                <i class="fs-4 bi-bootstrap"></i> <span
-                                    class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                            <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                                <li class="w-100">
-                                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span>
-                                        1</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span>
-                                        2</a>
-                                </li>
-                            </ul>
-                        </li>
+
                         <li>
                             <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                                 <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
@@ -128,41 +76,34 @@
                                 </li>
                             </ul>
                         </li>
-                        <li>
-                            <a href="#" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span>
-                            </a>
-                        </li>
                     </ul>
-                    <hr>
-                    <div class="dropdown pb-4">
-                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                            id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30"
-                                class="rounded-circle">
-                            <span class="d-none d-sm-inline mx-1">loser</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                            <li><a class="dropdown-item" href="#">New project...</a></li>
-                            <li><a class="dropdown-item" href="#">Settings</a></li>
-                            <li><a class="dropdown-item" href="#">Profile</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
             <div class="col py-3">
-                <h3>Left Sidebar with Submenus</h3>
-                <p class="lead">An example 2-level sidebar with collasible menu items. The menu functions like an
-                    "accordion" where only a single menu is be open at a time.</p>
-                <ul class="list-unstyled">
-                    <li>
-                        <h5>Responsive</h5> shrinks in width, hides text labels and collapses to icons only on mobile
-                    </li>
-                </ul>
+                <div class="col py-3">
+                    <div class="row">
+                        @foreach ($products as $prod)
+
+                            @if ($prod->category === 'Electronic')
+                                <div class="col-4">
+                                    <div class="card border-success mb-3" style="max-width: 18rem;">
+                                        <div class="card-header bg-transparent border-success">{{ $prod->category }}
+                                        </div>
+                                        <div class="card-body text-success">
+                                            <h4 class="card-title">{{ $prod->name }}</h4>
+                                            <h6 class="card-title">Stock: {{ $prod->stock }}</h6>
+
+                                        </div>
+                                        <div class="card-footer bg-transparent border-success">Cost:
+                                            ${{ $prod->price }}
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
     </div>
